3db592d54f619d451e84c0acd585c1f72ea30899
exodise0404 <exodise0404@gmail.com>
2025-01-10 21:27:26 +0800

    Add files via upload

--- module_16_3.py
+++ module_16_3.py
@@ -7,18 +7,18 @@ app = FastAPI()
 users = {'1': 'Имя: Example, возраст: 18'}
 
 @app.get('/users')
-async def get_all_messages(users: Annotated[int, Path(ge=1, le=100, description='Example', example='1')]):
+async def get_users(users: Annotated[int, Path(ge=1, le=100, description='Example', example='1')]):
     return users
 
 @app.post("/user/{username}/{age}")
-async def create_message(username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')],
+async def create_user(username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')],
                          age: Annotated[int, Path(ge=18, le=120, description='Enter age', example='24')]) -> str:
     new_id = str(int(max(users, key=int)) + 1)
     users[new_id] = f"Имя: {username}, возраст: {age}"
     return f"User {new_id} is registered"
 
 @app.put("/user/{user_id}/{username}/{age}")
-async def update_message(
+async def update_user(
         user_id: Annotated[str, Path(description='Enter user ID', example='1')],
         username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username", example="Urban User")],
         age: Annotated[int, Path(ge=18, le=120, description='Enter age', example='22')]) -> str:
